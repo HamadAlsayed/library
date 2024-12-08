d2d73dfeebb17394d257e5fae8b4ba95b0aff0bd
HamadAlsayed <hamad_sayed@outlook.com>
2024-12-08 14:13:54 +0200

    add button to add display form and add book

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-const myLibrary = [];
-
-function Book(title, author, pages, hasBeenRead)
+window.onload = function()
 {
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.hasBeenRead = hasBeenRead
-};
+    const myLibrary = [];
 
-function addBookToLibrary()
-{
-    
+    let addBookBtn = document.getElementById("add-book-btn");
+    let formCnt = document.getElementById("form-container");
+
+    function Book(title, author, pages, hasBeenRead)
+    {
+        this.title = title,
+        this.author = author,
+        this.pages = pages,
+        this.hasBeenRead = hasBeenRead
+    };
+
+    function addBookToLibrary()
+    {
+        let newBook = new Book(title, author, pages, hasBeenRead);
+        myLibrary.push(newBook);
+    }
+
+    addBookBtn.addEventListener("click", function() {
+        formCnt.style.display = "contents";
+    })
 }
