f457191e7d3bc26b62fbfe28c59659b6594d9441
HamadAlsayed <hamad_sayed@outlook.com>
2024-12-08 16:39:09 +0200

    Book form adds new Book to library array

--- script.js
+++ script.js
@@ -3,23 +3,37 @@ window.onload = function()
     const myLibrary = [];
 
     let addBookBtn = document.getElementById("add-book-btn");
-    let formCnt = document.getElementById("form-container");
+    let newBookForm = document.getElementById("new-book-form")
 
-    function Book(title, author, pages, hasBeenRead)
+    function Book(title, author, pages, read)
     {
         this.title = title,
         this.author = author,
         this.pages = pages,
-        this.hasBeenRead = hasBeenRead
+        this.read = read;
     };
 
     function addBookToLibrary()
     {
-        let newBook = new Book(title, author, pages, hasBeenRead);
-        myLibrary.push(newBook);
+        let title = document.getElementById("title").value;
+        let author = document.getElementById("author").value;
+        let pages = document.getElementById("pages").value;
+        let read = document.getElementById("read").value;
+        let newBook = new Book(title, author, pages, read);
+        console.log(newBook);
     }
 
     addBookBtn.addEventListener("click", function() {
-        formCnt.style.display = "block";
+        newBookForm.style.display = "block";
     })
+
+    newBookForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        addBookToLibrary();
+    })
+
+    function render()
+    {
+
+    }
 }
