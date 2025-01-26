a9fa9cc479d337ec3e9d7ca9cbd59b2ad29b125f
HamadAlsayed <hamad_sayed@outlook.com>
2025-01-26 19:43:41 +0200

    feat: addBookToLibrary
    A function that adds newly created books to myLibrary array

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+let bookForm = document.getElementById('new-book-form');
+let addBookBtn = document.getElementById('add-book-btn');
+
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+bookForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    
+})
+
+function createBook(title, author, pages, read)
+{
     return {
         title: title,
         author: author,
@@ -9,6 +19,22 @@ function Book(title, author, pages, read) {
     }
 }
 
-function addBookToLibrary() {
+function addBookToLibrary()
+{
+    let titleVal = document.getElementById('title').value,
+    authorVal = document.getElementById('author').value,
+    pages = document.getElementById('pages').value,
+    read = document.getElementById('read').value;
+
+    myLibrary.push(createBook(titleVal, authorVal, pages, read));
+}
+
+function render()
+{
 
 }
+
+
+addBookBtn.addEventListener('click', (e) => {
+    bookForm.style.display = 'block';
+})
