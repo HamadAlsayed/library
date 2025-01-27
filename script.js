4c3892a038525f607ac8e54da7dc60a1295fcf51
HamadAlsayed <hamad_sayed@outlook.com>
2025-01-27 11:21:50 +0200

    fix: read button now changes status
    Read button now changes the button's read status accordingly and its value in the array

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let bookForm = document.getElementById('new-book-form');
 let addBookBtn = document.getElementById('add-book-btn');
 let bookTable = document.getElementById('book-table');
+let checkboxes = document.querySelectorAll('.book-check')
 
 const myLibrary = [];
+
+myLibrary.push(createBook('Thinking, Fast and Slow', 'Daniel Kahneman', 416, false));
+myLibrary.push(createBook('A Profound Waste of Time', 'Caspian Whistler', 184, true))
+render();
 
 bookForm.addEventListener('submit', (e) => {
     // Prevent page from reloading 'Add Book' is clicked
@@ -25,9 +30,6 @@ function createBook(title, author, pages, read)
         author: author,
         pages: pages,
         read: read,
-        toggleRead() {
-            !read;
-        },
     }
 }
 
@@ -43,21 +45,36 @@ function addBookToLibrary()
 
 function render()
 {
-
+    // Clear current table rows except for header
     while (bookTable.rows.length > 1) {
         bookTable.deleteRow(1);
     }
 
-    // Loop through myLibrary array to dispaly new <td> elements
+    // Loop through myLibrary array to display new <td> elements
     for (i = 0; i < myLibrary.length; i++)
     {
         let newRow = document.createElement('tr'),
         tdTitle = addTextNode('td', myLibrary[i].title),
         tdAuthor = addTextNode('td', myLibrary[i].author),
         tdPages = addTextNode('td', myLibrary[i].pages),
-        tdRead = addTextNode('td', myLibrary[i].read);
+        tdRead = addTextNode('td', ''),
+        tdDelete = addTextNode('td', ''),
+        deleteBtn = addTextNode('button', 'Delete'),
+        readBtn = document.createElement('button');
+        deleteBtn.classList.add('delete-btn');
+        readBtn.classList.add('read-btn');
 
-        newRow.append(tdTitle, tdAuthor, tdPages, tdRead);
+        if (myLibrary[i].read) {
+            readBtn.innerHTML = 'Read';
+        } else {
+            readBtn.innerHTML = 'Unread';
+        }
+        
+
+        tdRead.appendChild(readBtn);
+        tdDelete.appendChild(deleteBtn);
+
+        newRow.append(tdTitle, tdAuthor, tdPages, tdRead, tdDelete);
         bookTable.appendChild(newRow);
         newRow.setAttribute('data-id', i);
     }
@@ -74,4 +91,21 @@ function addTextNode(type, text) {
 
 addBookBtn.addEventListener('click', (e) => {
     bookForm.style.display = 'block';
+});
+
+bookTable.addEventListener('click', e => {
+    let getNodeID = e.target.parentNode.parentNode.getAttribute('data-id');
+    if (e.target.matches('.read-btn')) {
+        if (myLibrary[getNodeID].read == true) {
+            myLibrary[getNodeID].read = false;
+            e.target.innerHTML = 'Unread';
+            console.log(myLibrary[getNodeID]);
+        } else {
+            myLibrary[getNodeID].read = true;
+            e.target.innerHTML = 'Read';
+            console.log(myLibrary[getNodeID]);
+        }
+    }
+
+
 })
